8fc78a34661d3870c02d69963880264845fcafed
Aaqib Parvez <aaqib29292@gmail.com>
2016-01-25 12:03:21 +0530

    done with corner cases of 0-key

--- js/calc.js
+++ js/calc.js
@@ -1,7 +1,23 @@
+function handleInput(key) {
+  $("#preview").append(key);
+}
+
+
+
+
+
 $(document).ready(function() {
   $('.key').click(function() {
-    var number = $(this).text();
-    $("#preview").append(number);
+    var key = $(this).text();
+    // corner case of 0 key
+    if(key == "0") {
+      if ($("#preview").html() != "0") {
+        handleInput(key);
+      }
+    }
+    else {
+      handleInput(key);
+    }
   });
 
 });
