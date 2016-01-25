35f8b9ffae529f8ffd19c05eaf76cbd86e0106a4
Aaqib Parvez <aaqib29292@gmail.com>
2016-01-25 18:32:56 +0530

    done with corner case of operators

--- js/calc.js
+++ js/calc.js
@@ -6,11 +6,27 @@ function previewContent() {
   return $("#preview").html();
 }
 
+function deleteLastChar() {
+  var preview = $("#preview").html();
+  var newPreview = preview.slice(0, -1);
+  $("#preview").html(newPreview);
+  // $("#preview").html(previewContent().slice(0, -1));
+}
+
+function keyIsOperator(key) {
+  return (["+", "-", "*", "/"].indexOf(key) != -1);
+}
+
+
+
+
+
+
 
 
 $(document).ready(function() {
   $('.key').click(function() {
-    var key = $(this).text();
+    var key = $(this).html();
     // corner case of 0 key
     if(key == "0") {
       if (previewContent() != "0") {
@@ -19,10 +35,20 @@ $(document).ready(function() {
     }
     // corner case of DEL key
     else if (key == "DEL") {
-      // var preview = $("#preview").html();
-      // var newPreview = preview.slice(0, -1);
-      // $("#preview").html(newPreview);
-      $("#preview").html(previewContent().slice(0, -1));
+      deleteLastChar()
+    }
+    else if (keyIsOperator(key))  {
+      // Successive operators corner case
+      var lastChar = previewContent().slice(-1);
+      if (keyIsOperator(lastChar)) {
+        // Delete the last operator from the preview
+        deleteLastChar();
+      }
+
+      // First operator in the preview corner case
+      if ((previewContent() != "") || (key == "-")) {
+        handleInput(key);
+      }
     }
     else {
       handleInput(key);
